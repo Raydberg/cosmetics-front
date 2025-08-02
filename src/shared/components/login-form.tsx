ef786550d101d8022c695f65ee39f5f41d3a5392
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-02 11:11:47 -0500

    Add framer-motion dependency and refactor LoginForm component to use new form structure

--- src/shared/components/login-form.tsx
+++ src/shared/components/login-form.tsx
@@ -1,51 +1,82 @@
 import { cn } from "@/core/lib/utils";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Link } from "react-router";
+import { LockKeyhole } from "lucide-react";
+import type { ComponentProps } from "react";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { email, z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+const formSchema = z.object({
+    email: z.email(),
+    password: z.string()
+})
+export const LoginForm = ({ className, ...props }: ComponentProps<"div">) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    })
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    }
 
-export function LoginForm({
-    className,
-    ...props
-}: React.ComponentProps<"div">) {
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card>
-                <CardHeader>
-                    <CardTitle>Acceso solo para administradores</CardTitle>
-                    <CardDescription>
-                        Ingresa tu correo y contraseña dada por el administrador
+                <CardHeader className="place-items-center">
+                    <LockKeyhole size={45} />
+                    <CardTitle >Acceso Administrativo</CardTitle>
+                    <CardDescription className="text-center">
+                        Ingresa tus credenciales dadas por el administrador para acceder al panel de administración
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form>
-                        <div className="flex flex-col gap-6">
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">Correo</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <div className="flex flex-col gap-6">
+
+                                <FormField control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Correo</FormLabel>
+                                            <FormControl>
+                                                <Input required placeholder="Email" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
                                 />
-                            </div>
-                            <div className="grid gap-3">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Contraseña</Label>
+
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Contraseña</FormLabel>
+                                            <FormControl>
+                                                <Input required placeholder="Ingrese su contraseña" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className="flex flex-col gap-3">
+                                    <Button type="submit" className="w-full">
+                                        Ingresar
+                                    </Button>
+                                    <Link to={"/"}>
+                                        Regresar
+                                    </Link>
                                 </div>
-                                <Input id="password" type="password" required />
                             </div>
-                            <div className="flex flex-col gap-3">
-                                <Button type="submit" className="w-full">
-                                    Inicio de Sesion
-                                </Button>
-                                <Link to={"/"}>
-                                    Regresar
-                                </Link>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </Form>
                 </CardContent>
             </Card>
         </div>
